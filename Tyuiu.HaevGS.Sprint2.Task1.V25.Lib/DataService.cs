07ba1379c5f8b81d87a22a92fcf7ae8fdd07d668
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-05 13:38:04 +0500

    Task1

---
+++ Tyuiu.HaevGS.Sprint2.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.HaevGS.Sprint2.Task1.V25.Lib
+{
+    public class DataService : ISprint2Task1V25
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a == b) | (c != d); //true
+            res[1] = (a < b) & (c > d);
+            res[2] = (a < b) || (c < d); // true
+            res[3] = (a > b) && (c > d);
+            res[4] = (!res[1]);
+            res[5] = (a > b) ^ (c < d); //false
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint2.Task1.V25/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.HaevGS.Sprint2.Task1.V25.Lib;
+
+DataService ds = new DataService();
+
+int a = 247;
+int b = 155;
+int c = 325;
+int d = 857;
+
+bool[] res = new bool[6];
+res = ds.GetLogicOperations(a, b, c, d);
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
