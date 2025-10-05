bdb450e8f04614ac79812cb9024e0f5d687f819e
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-05 13:03:44 +0500

    Task0

--- Tyuiu.HaevGS.Sprint2.Task0.V4.Lib/DataService.cs
+++ Tyuiu.HaevGS.Sprint2.Task0.V4.Lib/DataService.cs
@@ -10,12 +10,12 @@ namespace Tyuiu.HaevGS.Sprint2.Task0.V4.Lib
 
             res[0] = x == y;
             res[1] = x != y;
-            res[2] = y < x;
-            res[3] = y > x;
-            res[4] = y <= x;
-            res[5] = y >= x;
+            res[2] = x < y;
+            res[3] = x > y;
+            res[4] = x <= y;
+            res[5] = x >= y;
 
             return res;
         }
-    }
+    }   
 }
