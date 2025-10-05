33fc23c909cfc11f403a9041e1e2b1f7fe45246a
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-05 15:41:47 +0500

    Task2

---
+++ Tyuiu.HaevGS.Sprint2.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.HaevGS.Sprint2.Task2.V21.Lib;
+
+namespace Tyuiu.HaevGS.Sprint2.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = -4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint2.Task2.V21/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.HaevGS.Sprint2.Task2.V21.Lib;
+
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+DataService ds = new DataService();
+
+bool res = ds.CheckDotInShadedArea(x, y);
+
+if (res)
+{
+    Console.WriteLine("штрих");
+}
+else
+{
+    Console.WriteLine("нет штрих");
+}
+Console.ReadKey();
