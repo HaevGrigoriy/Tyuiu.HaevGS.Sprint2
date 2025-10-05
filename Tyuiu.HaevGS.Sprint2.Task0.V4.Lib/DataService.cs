bb901b4c1f2df24c5d3db43fd8d0c1106817aa25
User <User@DESKTOP-NRCIQEV>
2025-10-05 12:55:19 +0500

    Add project files.

---
+++ Tyuiu.HaevGS.Sprint2.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.HaevGS.Sprint2.Task0.V4.Lib
+{
+    public class DataService : ISprint2Task0V4
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y;
+            res[1] = x != y;
+            res[2] = y < x;
+            res[3] = y > x;
+            res[4] = y <= x;
+            res[5] = y >= x;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint2.Task0.V4/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.HaevGS.Sprint2.Task0.V4.Lib;
+
+DataService ds = new DataService();
+
+int x = 333;
+int y = 324;
+bool[] res = new bool[6];
+
+res = ds.GetCompareOperations(x, y);
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
