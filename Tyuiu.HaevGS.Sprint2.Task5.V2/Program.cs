473a5ae0e505df3fdf4377300d74e325fdd0389c
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-06 18:01:39 +0500

    Task5

---
+++ Tyuiu.HaevGS.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.HaevGS.Sprint2.Task5.V2.Lib;
+
+DataService ds = new DataService();
+
+int month = Convert.ToInt32(Console.ReadLine());
+
+string res;
+
+if ((month < 1) || (month > 12))
+{
+    res = "Неправильно";
+}
+else
+{
+    res = ds.FindMonthSeason(month);
+}
+Console.WriteLine(res);
+Console.ReadKey();
