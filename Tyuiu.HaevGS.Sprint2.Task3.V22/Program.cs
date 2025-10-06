6875d203cf3f8fd06a817359854c1f35fb6c5a72
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-06 10:53:01 +0500

    Task3

--- Tyuiu.HaevGS.Sprint2.Task3.V22/Program.cs
+++ Tyuiu.HaevGS.Sprint2.Task3.V22/Program.cs
@@ -6,5 +6,5 @@ DataService ds = new DataService();
 double x = Convert.ToDouble(Console.ReadLine());
 double res = ds.Calculate(x);
 
-Console.WriteLine(res);
+Console.WriteLine(Math.Round(res, 3));
 Console.ReadKey();
